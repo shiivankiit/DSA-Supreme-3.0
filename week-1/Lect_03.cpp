ac0e4c6726791f17f3985d121e98d0d8d44f0bdb
shiivankiit <shivankit7557@gmail.com>
2026-02-03 20:09:28 +0530

    User-input

---
+++ week-1/Lect_03.cpp
@@ -0,0 +1,12 @@
+//User-input in c++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout <<"Enter your age" << endl;
+    cin>>age;
+    cout <<"Your age is"<<age << endl;
+    return 0;
+}
