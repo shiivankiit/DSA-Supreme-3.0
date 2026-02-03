27318c9088e629b34bec2e2530f5ef7980f675e0
shiivankiit <shivankit7557@gmail.com>
2026-02-03 18:58:44 +0530

    Variable & Data-types

---
+++ week-1/Lect_02.cpp
@@ -0,0 +1,46 @@
+// Variable and Data-types
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    //variable define
+    int age=25;
+    cout<<"My age is "<<age<<endl;
+    //Manipulatio or updation.
+    age=101;
+    cout<<age<<endl;
+    //declaration
+    int count;//contains a random value also called garbage value.
+
+    // Data-types.
+    //int
+    int weight=23;
+    //float
+    float height=5.7;
+    //char
+    char alphabet ='w';
+    //double
+    double pi=3.1473;
+    //bool
+    bool isMale=true;
+    bool ischild=1;
+    bool isfemale=false;
+    bool isgirl=0;
+
+    cout << weight <<endl;
+    cout << height <<endl;
+    cout << alphabet << endl;
+    cout << pi <<endl;
+    cout << isMale<<endl;
+    cout << ischild<<endl;
+    cout << isfemale <<endl;
+    cout << isgirl <<endl;
+
+    
+
+    
+   
+
+    return 0;
+}
