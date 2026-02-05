031cfb0286c5229f82870207cfafe80a2c635c2d
shiivankiit <shivankit7557@gmail.com>
2026-02-05 22:39:46 +0530

    Ternary-operator

---
+++ week-1/Lect_06.cpp
@@ -0,0 +1,11 @@
+// Ternary Operator.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 22;
+    (age > 18) ? cout << "can vote" : cout << "can'not vote";
+    return 0;
+}
