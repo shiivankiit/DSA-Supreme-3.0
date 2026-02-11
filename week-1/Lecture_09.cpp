b3f9ab65aa4028518da843a2e0878881dd8822ec
shiivankiit <shivankit7557@gmail.com>
2026-02-11 23:56:51 +0530

    Operators in c++

---
+++ week-1/Lecture_09.cpp
@@ -0,0 +1,12 @@
+//Operators in C++
+//An Operator is a symbol that operates on a value to
+//perform specific mathematical or logical computation
+//They form the foundation of any programming language
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Unary opeerator.
+    
+    return 0;
+}
